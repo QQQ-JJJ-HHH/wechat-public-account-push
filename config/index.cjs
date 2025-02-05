d2188fa023140a7a08ecd63825159fe17870a3ef
QQQ-JJJ-HHH <1060617697@qq.com>
2025-02-05 15:22:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,12 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  //USE_PASSAGE: 'push-deer',
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx1ad5d689b10bc6b7',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '53bfc3b3588a73f2d38296bf4475f4dc',
+  APP_SECRET: '',
 
   PROVINCE: '浙江',
   CITY: '金华',
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ''//'PDU33716Txn4HmouiJKxVi8ubu2bFskp9uIcSmvcS',
+      id: 'PDU33716Txn4HmouiJKxVi8ubu2bFskp9uIcSmvcS',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
